470beac47e8c5ad84b1e0abf0f40195b45f696ea
Sagar <sagardevelops15@gmail.com>
2023-04-18 12:11:08 +0530

    Added input field for apiendpoint on ConfigEditor

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -3,14 +3,23 @@ import { InlineField, Input, SecretInput } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { MyDataSourceOptions, MySecureJsonData } from '../types';
 
-interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
+interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> { }
 
 export function ConfigEditor(props: Props) {
   const { onOptionsChange, options } = props;
+
   const onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
     const jsonData = {
       ...options.jsonData,
       path: event.target.value,
+    };
+    onOptionsChange({ ...options, jsonData });
+  };
+
+  const onAPIEndPointChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const jsonData = {
+      ...options.jsonData,
+      apiEndpoint: event.target.value,
     };
     onOptionsChange({ ...options, jsonData });
   };
@@ -60,6 +69,14 @@ export function ConfigEditor(props: Props) {
           width={40}
           onReset={onResetAPIKey}
           onChange={onAPIKeyChange}
+        />
+      </InlineField>
+      <InlineField label="API Endpoint" labelWidth={12}>
+        <Input
+          onChange={onAPIEndPointChange}
+          value={jsonData.apiEndpoint || ''}
+          placeholder="API Endpoint"
+          width={40}
         />
       </InlineField>
     </div>
