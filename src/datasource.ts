719681252c757dadc17d4fe85172ad76487cfd2c
Jasmin Makasana <jasminmakasana1991@gmail.com>
2023-11-08 14:52:52 +0530

    Update: query is edited based on the variable.

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,6 +1,6 @@
-import { DataSourceInstanceSettings, CoreApp, DataQueryRequest, DataQueryResponse } from '@grafana/data';
+import { DataSourceInstanceSettings, CoreApp, DataQueryRequest, DataQueryResponse, ScopedVars } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
-
+import { services } from './service';
 import { MyQuery, MyDataSourceOptions, DEFAULT_QUERY } from './types';
 import { Observable } from 'rxjs';
 
@@ -9,11 +9,50 @@ export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptio
     super(instanceSettings);
   }
 
+  findParam(paramName: string, url: string) {
+    if (!url) {
+      url = location.href;
+    }
+    paramName = paramName.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    const regexS = "[\\?&]" + paramName + "=([^&#]*)";
+    const regex = new RegExp(regexS);
+    const results = regex.exec(url);
+    return results == null ? "" : results[1];
+  }
+
+  async getCloudElements(id: string){
+    const res = await services.getCloudElements(id);
+    let query = {};
+    if (res && res[0]) {
+      const cloudElement = res[0];
+      query = {
+        "elementType": cloudElement.elementType,
+        "elementId": parseInt(id, 10),
+        "cloudIdentifierName": cloudElement.instanceName,
+        "cloudIdentifierId": cloudElement.instanceId,
+        "environmentId": parseInt(id, 10),
+      };
+    }
+    return query;
+  };
+
   getDefaultQuery(_: CoreApp): Partial<MyQuery> {
     return DEFAULT_QUERY
   }
 
-  query(request: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
+  query(request: DataQueryRequest<MyQuery>): Observable<DataQueryResponse>{
+    const id = this.findParam("var-elementId", window.location.href);
+    let query = {};
+    if(id){
+      query = this.getCloudElements(id);
+    }
+    let targets = request.targets;
+    for(let i=0; i<targets.length; i++){
+      targets[i] = {
+        ...targets[i],
+        ...query
+      }
+    }
     return super.query(request);
   }
 }
--- src/service/index.ts
+++ src/service/index.ts
@@ -2,8 +2,9 @@ const BASE_URL = "http://34.199.12.114:6057";
 const GET_CLOUD_ELEMENT = "/api/cloud-element/search";
 
 export const services = {
-    getCloudElements: function(id: string) {
+    getCloudElements: async function(id: string) {
         return fetch(`${BASE_URL}${GET_CLOUD_ELEMENT}?id=${id}`)
         .then(response => response.json())
+        .then(response => {return response});
     }
 };
