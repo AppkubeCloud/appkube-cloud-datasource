dd706f27b077e0b1967cad50a0e465b3f94d9490
Jasmin Makasana <jasminmakasana1991@gmail.com>
2023-11-08 16:55:17 +0530

    Fix: issue with query fixed.

--- src/datasource.ts
+++ src/datasource.ts
@@ -2,7 +2,8 @@ import { DataSourceInstanceSettings, CoreApp, DataQueryRequest, DataQueryRespons
 import { DataSourceWithBackend } from '@grafana/runtime';
 import { services } from './service';
 import { MyQuery, MyDataSourceOptions, DEFAULT_QUERY } from './types';
-import { Observable } from 'rxjs';
+import { Observable, from, map, mergeMap } from 'rxjs';
+import { flatMap } from 'lodash';
 
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
@@ -20,39 +21,53 @@ export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptio
     return results == null ? "" : results[1];
   }
 
-  async getCloudElements(id: string){
-    const res = await services.getCloudElements(id);
-    let query = {};
-    if (res && res[0]) {
-      const cloudElement = res[0];
-      query = {
-        "elementType": cloudElement.elementType,
-        "elementId": parseInt(id, 10),
-        "cloudIdentifierName": cloudElement.instanceName,
-        "cloudIdentifierId": cloudElement.instanceId,
-        "environmentId": parseInt(id, 10),
-      };
-    }
-    return query;
+  getCloudElements(id: string) {
+    return from(services.getCloudElements(id).then(res => {
+      let query = {};
+      if (res && res[0]) {
+        const cloudElement = res[0];
+        query = {
+          "elementType": cloudElement.elementType,
+          "elementId": parseInt(id, 10),
+          "cloudIdentifierName": cloudElement.instanceName,
+          "cloudIdentifierId": cloudElement.instanceId,
+          "environmentId": parseInt(id, 10),
+        };
+      }
+      return query;
+    }));
   };
 
   getDefaultQuery(_: CoreApp): Partial<MyQuery> {
     return DEFAULT_QUERY
   }
 
-  query(request: DataQueryRequest<MyQuery>): Observable<DataQueryResponse>{
+  query(request: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
     const id = this.findParam("var-elementId", window.location.href);
-    let query = {};
-    if(id){
-      query = this.getCloudElements(id);
+    if (id) {
+      // return this.getCloudElements(id).pipe(query => {
+      //   let targets = request.targets;
+      //   for (let i = 0; i < targets.length; i++) {
+      //     targets[i] = {
+      //       ...targets[i],
+      //       ...query
+      //     }
+      //   }
+      //   return super.query(request);
+      // });
+      return this.getCloudElements(id).pipe(
+        mergeMap((query: object) => {
+          let targets = request.targets;
+          for (let i = 0; i < targets.length; i++) {
+            targets[i] = {
+              ...targets[i],
+              ...query
+            }
+          }
+          return super.query(request);
+        }));
+    } else {
+      return super.query(request);
     }
-    let targets = request.targets;
-    for(let i=0; i<targets.length; i++){
-      targets[i] = {
-        ...targets[i],
-        ...query
-      }
-    }
-    return super.query(request);
   }
 }
--- src/service/index.ts
+++ src/service/index.ts
@@ -2,9 +2,8 @@ const BASE_URL = "http://34.199.12.114:6057";
 const GET_CLOUD_ELEMENT = "/api/cloud-element/search";
 
 export const services = {
-    getCloudElements: async function(id: string) {
+    getCloudElements: function(id: string) {
         return fetch(`${BASE_URL}${GET_CLOUD_ELEMENT}?id=${id}`)
-        .then(response => response.json())
-        .then(response => {return response});
+        .then(response => response.json());
     }
 };
